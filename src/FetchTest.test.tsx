6e5ce947fde6a87c06b0b2d88baf763c014fd6af
허제인 <codingjamee@gmail.com>
2025-01-13 23:26:04 +0900

    버튼 클릭시 불러온 데이터 및 서버 에러 발생시 에러 문구 노출 여부 확인

--- src/FetchTest.test.tsx
+++ src/FetchTest.test.tsx
@@ -1,5 +1,5 @@
 import { describe, test, it, expect, beforeEach } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { FetchComponent } from "./FetchComponent";
 import { setupServer } from "msw/node"; // browser 대신 node를 사용
 import { http, HttpResponse } from "msw";
@@ -35,5 +35,27 @@ describe("FetchComponent 테스트", () => {
   it("데이터를 불러오기 전에는 기본 문구가 뜬다", async () => {
     const nowLoading = await screen.findByText(/불러온 데이터가 없습니다./);
     expect(nowLoading).toBeInTheDocument();
+  });
+
+  it("버튼 클릭 시 데이터 불러온다", async () => {
+    const button = screen.getByRole("button", { name: /1번/ });
+    fireEvent.click(button);
+
+    const data = await screen.findByText(MOCK_TODO_RESPONSE.title);
+    expect(data).toBeInTheDocument();
+  });
+
+  it("버튼 클릭 후 서버 에러가 발생하면 에러문구를 노출한다", async () => {
+    server.use(
+      http.get("/todos/:id", () => {
+        return new HttpResponse(null, { status: 503 });
+      })
+    );
+
+    const button = screen.getByRole("button", { name: /1번/ });
+    fireEvent.click(button);
+
+    const error = await screen.findByText(/에러가 발생했습니다/);
+    expect(error).toBeInTheDocument();
   });
 });
